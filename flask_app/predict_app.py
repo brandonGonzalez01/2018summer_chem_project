43fdffbe7a970eb6be92b06e8e3b4671f7baf385
brandonGonzalez01 <brg029@ucsd.edu>
2018-09-11 21:29:52 -0700

    adding the main components of the web app

---
+++ flask_app/predict_app.py
@@ -0,0 +1,175 @@
+
+from flask import request, jsonify, Flask
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from rdkit.Chem.rdmolops import RDKFingerprint
+from rdkit import DataStructs
+from rdkit.Chem import AllChem
+import numpy as np
+import io
+import rdkit
+import keras
+from keras.models import Sequential
+from keras.models import load_model
+import pubchempy as pcp
+
+app = Flask(__name__)
+app.debug = False
+def get_model():
+    global model
+    global excipientModel
+    excipientModel = load_model('chem_E_model.h5')
+    model = load_model('chem_model.h5')
+    x = preprocess_SMILE("C")
+    model.predict(x)
+    excipientModel.predict(x)    
+    print(" * Model loaded!")
+
+def get_weight(smiles):
+    #check if smiles was single SMILES input
+    if(type(smiles) is str):
+        return Chem.Descriptors.MolWt(Chem.MolFromSmiles(smiles))
+    #check if SMILES smiles was from Excipient option
+    elif(smiles[0]=='E'):
+        return Chem.Descriptors.MolWt(Chem.MolFromSmiles(smiles[1]))
+    else:
+        #else the smiles is an array of smiles
+        weights = []
+        for i in range(len(smiles)):
+            mol = Chem.MolFromSmiles(smiles[i])
+            wieght = Chem.Descriptors.MolWt(mol)
+            weights.append(wieght)
+        return weights
+
+def get_name(smiles):
+    if(type(smiles) is str):
+        try:
+            syn = pcp.get_synonyms(smiles,'smiles')[0]['Synonym']
+            print(syn)
+            return syn[0]
+        except:
+            try:
+                return pcp.get_properties('IUPACName',smiles,'smiles')[0]['IUPACName']
+            except:
+                return smiles
+    else:
+        names = []
+        for i in range(len(smiles)):
+            if(smiles[i]==''):
+                continue
+            else:
+                try:
+                    syn = pcp.get_synonyms(smiles[i],'smiles')[0]['Synonym'][0]
+                    print('syn @ ',i,' : ',syn)
+                    names.append(syn)
+                except:
+                    try:
+                        IUPAC = pcp.get_properties('IUPACName',smiles[i],'smiles')
+                        print(f"{i}-{smiles[i]} , IUPAC used instead {IUPAC}" )
+                        names.append(IUPAC[0]['IUPACName'][0])
+                    except:
+                        names.append(smiles[i][:10])
+    return names
+
+def preprocess_SMILE(smiles):
+    print(smiles)
+    fpArray = []
+    #length = number of bits the MorganFingerprint has
+    length = 512
+    #print('length: ',len(smiles),smiles)
+    if(type(smiles) is str or smiles[0]=="E" ):
+        mol = ""
+        
+        #check if excipient option is selected
+        if smiles[0]=="E":
+            mol = Chem.MolFromSmiles(smiles[1])
+        else:
+            mol = Chem.MolFromSmiles(smiles)
+        #error check
+        if(mol is None):
+            print('mol invalid: ',smiles)
+        else:
+            fp = AllChem.rdmolops.RDKFingerprint(mol,fpSize = length)
+            fpArray.append(fp)  
+    
+    else:#else the smiles in an array for the water solubility model
+        #this split function bc the first array SMILE has an abberanr " in it 
+        print("length of smiles: ",len(smiles))
+        for i in range(len(smiles)):
+            mol = Chem.MolFromSmiles(smiles[i])
+            if(mol is None):
+                continue
+            else:
+                fp = AllChem.GetMorganFingerprintAsBitVect(mol,3, nBits = length)
+                fpArray.append(fp)
+    #convert the fingerprint array into a numpy array
+    X = np.array(fpArray) 
+    print(X)
+    return X
+
+print("* Loading model...")
+
+
+get_model()
+
+@app.route("/predict",methods=['POST'])
+
+def predict():
+
+    message = request.get_json(force=True)
+    print(message)
+    #check if the chosen option was to use the Excipient model
+    keys = list(message.keys())
+    if('toBeNamed' in keys ):
+        print(message['toBeNamed'])
+        print('success')
+        encoded_smile=message['toBeNamed']
+        name = get_name(encoded_smile)
+        response = {
+            'names': name
+        }
+        return jsonify(response)
+
+    elif(message['mol'][0] and message['mol'][0]=="E"):
+        print(message['mol'][0])
+        encoded_smile= message['mol'][1]
+        
+        #get weight properties of the SMILES
+        weight = get_weight(encoded_smile)
+        
+        #get the name of the SMILES in iupac
+        name = get_name(encoded_smile)
+        
+        processed_smile = preprocess_SMILE(encoded_smile)
+        prediction = excipientModel.predict(processed_smile)
+        print(prediction.tolist())
+        response={
+            'prediction': prediction.tolist(),
+            'weight' : weight,
+            'name' : name
+        }
+        return jsonify(response)
+    else:     
+        #else this model was the solubility in water model
+        encoded_smile = message['mol']
+        
+        #get the weights
+        weight = get_weight(encoded_smile)
+        
+        #get the names of the encoded_smile smiles list
+        name = get_name(encoded_smile)
+
+        processed_smile = preprocess_SMILE(encoded_smile)
+        prediction = model.predict(processed_smile)
+        print(prediction.tolist())
+        response = {
+                'prediction' : prediction.tolist(),     
+                'weight' : weight,
+                'name' : name,
+            }
+        print(response)
+        return jsonify(response)
+            
+    
+
+    
---
+++ flask_app/sample_app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/sample')
+def running():
+    return 'Flask is running!'
